d8044628b7759d64c7dcabc6e36e0a461de8ffc1
U-K-P <ukpal777@gmail.com>
2022-05-02 23:11:02 +0530

    remove comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -25,11 +20,6 @@ class User extends Authenticatable
         'token_expires_on'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
@@ -38,11 +28,6 @@ class User extends Authenticatable
         'token_expires_on'
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
